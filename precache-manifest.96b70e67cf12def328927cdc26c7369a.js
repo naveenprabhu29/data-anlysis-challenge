181835ab5d9670db2628122480b5419eef44b566
Naveen Prabhu <naveenprabhuje@gmail.com>
2020-11-30 16:05:57 +0530

    Updates

---
+++ precache-manifest.96b70e67cf12def328927cdc26c7369a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44923ed6e47cba8c747f3c2be309675",
+    "url": "/thenp/index.html"
+  },
+  {
+    "revision": "07e65d2d8563a3001aa0",
+    "url": "/thenp/static/css/main.643118fd.chunk.css"
+  },
+  {
+    "revision": "8dd9cab60f71f034910a",
+    "url": "/thenp/static/js/2.d79dac80.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/thenp/static/js/2.d79dac80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e65d2d8563a3001aa0",
+    "url": "/thenp/static/js/main.79dbb3b9.chunk.js"
+  },
+  {
+    "revision": "6fb41a3f512b09c6de8e",
+    "url": "/thenp/static/js/runtime-main.95cbca6b.js"
+  },
+  {
+    "revision": "9f50dbc73cbc1ab914e7837ce10460a1",
+    "url": "/thenp/static/media/circle-cropped.9f50dbc7.png"
+  }
+]);
