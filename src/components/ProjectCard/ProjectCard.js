a77b877e814da420efe0ea0298c68a727a6e46da
Naveen Prabhu <naveenprabhuje@gmail.com>
2020-12-03 08:31:32 +0530

    after flexing

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -7,23 +7,32 @@ import './project-card.css';
 export const ProjectCard = ({proj}) => {
     if(proj.status==="1"){
     return (
-        <div key={proj} className="project-card">
+<div> <div key={proj} className="project-card">
+          
          <h4 style={{color:"steelblue",fontSize:"25px"}}>{proj.name}</h4>
        
+  
          {proj.tech}<br/><br/>
          {proj.desc}
          <h5 ><a style={{color:"#16697a",textDecoration:"none"}} href={proj.web}>View Project </a></h5>
          <h5 ><a style={{color:"#16697a",textDecoration:"none"}} href={proj.source}>View Project Code </a></h5>
+
         
         </div>
+        <br/>
+         <hr style={{borderStyle:"none",borderTopStyle:"dotted",borderWidth:"5px",borderColor:"grey",width:"40%"}}/>
+        <br/></div>
+       
     )
     }
     else{
         return(
-            <div key={proj} className="project-card">
+            <div style={{display:"block"}}> <div key={proj} className="project-card">
          <h4 style={{color:"steelblue",fontSize:"25px"}}>{proj.name}</h4>
             <h4 style={{color:'#0a043c'}}>Project On Going</h4>
         </div>
+       </div>
+           
         )
     }
 }
